48f3b9820e1d6197bbe05fad26f18ac5d5125194
Panait Andrei <andrei.panait@teamnet.ro>
2021-07-05 20:58:52 +0300

    modified messages for multiple system out prints

--- src/ro/teamnet/Main.java
+++ src/ro/teamnet/Main.java
@@ -3,9 +3,9 @@ package ro.teamnet;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("good bye");
-	System.out.print("good bye");
-	System.out.print("good bye");
-	System.out.print("good bye");
+	System.out.print("good bye 1");
+	System.out.print("good bye 2");
+	System.out.print("good bye 3");
+	System.out.print("good bye 4");
     }
 }
